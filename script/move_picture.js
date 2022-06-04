41dd7bf3781ec43deeca4c036b61b85918a1c803
Nia <verusnikus@gmail.com>
2022-06-04 11:15:32 +0300

    Добавление проверки на выход за рамку

--- script/move_picture.js
+++ script/move_picture.js
@@ -1,18 +1,23 @@
 const img_kitty = document.querySelector(".kitty");
 const container = document.getElementById("container");
 let img_height = img_kitty.clientHeight, img_width = img_kitty.clientWidth;
-
+let container_height = container.clientHeight, container_width = container.clientWidth;
+let limit_right = container_width - img_width;
+let limit_up = container_height - img_height;
 
 img_kitty.addEventListener("mouseenter", (ev) => { MouseEnter(ev) });
 
 function MouseEnter(e) {
-    if (e.offsetX <= 3) {
-        img_kitty.style.left = (img_kitty.offsetLeft + 10) + "px";
-    } else if (e.offsetX >= img_width - 3) {
-        img_kitty.style.left = (img_kitty.offsetLeft - 10) + "px";
-    } else if (e.offsetY <= 3) {
-        img_kitty.style.top = (img_kitty.offsetTop + 10) + "px";
-    } else if (e.offsetY >= img_height - 3) {
-        img_kitty.style.top = (img_kitty.offsetTop - 10) + "px";
+    let ev_offsetX = e.offsetX, ev_offsetY = e.offsetY;
+    let img_offsetLeft = img_kitty.offsetLeft,  img_offsetTop = img_kitty.offsetTop;
+    
+    if (ev_offsetX <= 3 && img_offsetLeft < limit_right) {
+        img_kitty.style.left = (img_offsetLeft + 10) + "px";
+    } else if (ev_offsetX >= img_width - 3 && img_offsetLeft > 0) {
+        img_kitty.style.left = (img_offsetLeft - 10) + "px";
+    } else if (ev_offsetY <= 3 && img_offsetTop < limit_up) {
+        img_kitty.style.top = (img_offsetTop + 10) + "px";
+    } else if (ev_offsetY >= img_height - 3 && img_offsetTop > 0) {
+        img_kitty.style.top = (img_offsetTop - 10) + "px";
     }
 }
